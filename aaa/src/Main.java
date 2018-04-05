50a9bf80f5e6b3960557ac7c4756638789aa3630
hansen <623229800@qq.com>
2018-04-05 22:21:42 +0800

    add aaa

---
+++ aaa/src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by chenhansen on 2018/4/5.
+ */
+public class Main {
+    public static void main(String[] args){
+
+    }
+
+    public static void print(){
+        System.out.println("hello");
+    }
+
+}
---
+++ aaa/src/Other.java
@@ -0,0 +1,8 @@
+/**
+ * Created by chenhansen on 2018/4/5.
+ */
+public class Other {
+    public static void main(String[] args){
+
+    }
+}
